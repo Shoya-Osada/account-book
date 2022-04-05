ee1b1e25cbe4460a5c60e182974179908392cb31
Shoya-Osada <s.oauspicious@gmail.com>
2022-04-05 16:12:34 +0900

    production用の設定を追加

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,11 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
+const isProd = process.env.NODE_ENV === 'production'
+
+module.exports = {
   transpileDependencies: [
-    'vuetify',
-  ]
-})
+    'vuetify'
+  ],
+  publicPath: isProd ? '/リポジトリ名' : 'https://github.com/Shoya-Osada/account-book.git',
+  outputDir: 'docs',
+  filenameHashing: false,
+  productionSourceMap: false
+}
